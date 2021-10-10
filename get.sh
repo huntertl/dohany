4d2b070e9087a2255848529d4fb859430889b259
hunter.lippmann.com <hunter.lippmann.com>
2021-10-10 17:37:44 -0400

    iterate over 10 variants, not 3

--- get.sh
+++ get.sh
@@ -7,13 +7,17 @@ DEFAULT_OUTFILE='products.csv'
 
 for url in $(cat urls | grep -v '#'); do
 	safe_url=$(python3 get_url.py "$url")
+	URL_OUTFILE="files/$safe_url.csv"
 	echo "$url" "$safe_url"
 	python3 shopify.py --csv --elliot-template $url
+	
+	# check if files are different, move to $URL_OUTFILE to timestamp version if they are
 	sha_current=$(sha1sum "$DEFAULT_OUTFILE" | cut -d' ' -f1)
-	sha_last=$(sha1sum "files/$safe_url.csv" | cut -d' ' -f1)
+	sha_last=$(sha1sum "$URL_OUTFILE" | cut -d' ' -f1)
 	if [[ "$sha_current" != "$sha_last" ]]; then
-		last_modified_ts=$(date -r files/$safe_url.csv +"%s")
-		mv files/$safe_url.csv files/$safe_url\_$last_modified_ts.csv
+		echo moving
+		last_modified_ts=$(date -r "$URL_OUTFILE" +"%s")
+		mv $URL_OUTFILE files/$safe_url\_$last_modified_ts.csv
 	fi
-	mv products.csv files/$safe_url.csv
+	mv $DEFAULT_OUTFILE $URL_OUTFILE
 done
