27f714997c489f4839d064e2ca65eb4dd20a6042
hunter.lippmann.com <hunter.lippmann.com>
2021-10-10 15:59:27 -0400

    use elliot-template instead

--- get.sh
+++ get.sh
@@ -4,21 +4,10 @@
 # ia="https://web.archive.org/save"
 # now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-# # wget $url -O files/$now.html -o log/$now.log
-# wget $url -O file.html
-# #curl -s $ia/$url > /dev/null
-
-# #source .venv/bin/activate
-# python3 save_json.py
-# cat _file.json | jq '.' > file.json
-# rm _file.json
-
-
-
 for url in $(cat urls | grep -v '#'); do
    echo $url
 	 safe_url=$(python3 get_url.py "$url")
 	 echo "$url" "$safe_url"
-	 python3 shopify.py --csv $url
+	 python3 shopify.py --csv --elliot-template-1 $url
 	 mv products.csv files/$safe_url.csv
 done
--- get_url.py
+++ get_url.py
@@ -3,3 +3,7 @@ import sys
 from urllib.parse import urlparse
 url = sys.argv[1]
 print(urlparse(url).netloc.replace('.','_'))
+
+
+
+# echo 'https://docs.python.org/3.6/library/urllib.parse.html' | python3 -c 'import sys; from urllib.parse import urlparse; url = sys.argv[0]; print(urlparse(url).netloc)'
