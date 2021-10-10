93cffe387793a771b4a205f245abaa3e585e6138
hunter.lippmann.com <hunter.lippmann.com>
2021-10-10 16:57:26 -0400

    move updated files to ts.csv, run once an hour

--- get.sh
+++ get.sh
@@ -1,13 +1,19 @@
 # #!/bin/bash
 # mkdir -p files log
-# url="https://www.equityapartments.com/washington-dc/gallery-place-mt-vernon-triangle/425-mass-apartments"
 # ia="https://web.archive.org/save"
 # now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
+DEFAULT_OUTFILE='products.csv'
+
 for url in $(cat urls | grep -v '#'); do
-   echo $url
-	 safe_url=$(python3 get_url.py "$url")
-	 echo "$url" "$safe_url"
-	 python3 shopify.py --csv --elliot-template $url
-	 mv products.csv files/$safe_url.csv
+	safe_url=$(python3 get_url.py "$url")
+	echo "$url" "$safe_url"
+	python3 shopify.py --csv --elliot-template $url
+	sha_current=$(sha1sum "$DEFAULT_OUTFILE" | cut -d' ' -f1)
+	sha_last=$(sha1sum "files/$safe_url.csv" | cut -d' ' -f1)
+	if [[ "$sha_current" != "$sha_last" ]]; then
+		last_modified_ts=$(date -r files/$safe_url.csv +"%s")
+		mv files/$safe_url.csv files/$safe_url\_$last_modified_ts.csv
+	fi
+	mv products.csv files/$safe_url.csv
 done
